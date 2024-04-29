d769454bca242749cc96709281366121dd89edfe
Laban G <guildeur@hotmail.fr>
2024-04-29 10:02:41 +0200

    Data base setup

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  process.env.DATABASE_URL || 'mongodb+srv://UserTest:zerOVCUY1jqJuXfC@cluster0.cyg8xg3.mongodb.net/Cluster0?retryWrites=true&w=majority'
 
 module.exports = async () => {
   try {
